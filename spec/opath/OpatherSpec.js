e9ecb11ea4b88d530d5cb61dfe5659eb38dc0c92
Brian Graham <drakain@gmail.com>
2016-08-23 19:22:40 -0400

    Boostraps testing.

---
+++ spec/opath/OpatherSpec.js
@@ -0,0 +1,10 @@
+describe("Opather", function() {
+  var Opather = require('../../lib/opather');
+
+  beforeEach(function() {
+  });
+
+  it("should be defined", function() {
+    expect(Opather).toBeDefined();
+  });
+});
